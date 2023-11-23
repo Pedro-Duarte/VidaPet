114bd2ecb1f69f09aa277db5ec4d8d033b938179
Pedro Souto Duarte Junior <36248800+Pedro-Duarte@users.noreply.github.com>
2023-11-23 00:05:35 -0300

    formulario

--- src/components/contatoFormulario.jsx
+++ src/components/contatoFormulario.jsx
@@ -10,6 +10,13 @@ const Formulario = () => {
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
 
+  // Estados para armazenar a condição dos campos, se clicado ou não
+  const [nameClick, setNameClick] = useState(false);
+  const [emailClick, setEmailClick] = useState(false);
+  const [subjectClick, setSubjectClick] = useState(false);
+  const [messageClick, setMessageClick] = useState(false);
+
+
   // Funções de alteração para atualizar os estados conforme os usuários interagem com os campos
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -27,31 +34,61 @@ const Formulario = () => {
     setMessage(e.target.value);
   };
 
+  // Funções de alteração para atualizar os estados conforme os usuários clicam nos campos
+  const handleNameClick = () => {
+    setNameClick(true);
+  };
+  const handleNameClickBlur = () => {
+    setNameClick(false);
+  };
+
+
+  const handleEmailClick = () => {
+    setEmailClick(true);
+  };
+  const handleEmailClickBlur = () => {
+    setEmailClick(false);
+  };
+
+  const handleSubjectClick = () => {
+    setSubjectClick(true);
+  };
+  const handleSubjectClickBlur = () => {
+    setSubjectClick(false);
+  };
+
+  const handleMessageClick = () => {
+    setMessageClick(true);
+  };
+  const handleMessageClickBlur = () => {
+    setMessageClick(false);
+  };
+
   // Renderização do componente
   return (
     <StyledFormulario>
 
-      <div className={`form-group ${name ? 'focused' : ''}`}>
+      <div className={`form-group ${name ? 'focused' : nameClick === true ? 'focused' : ''}`}>
         <label htmlFor="name" onSelect={handleNameChange}>Nome</label>
-        <input type="text" id="name" value={name} onChange={handleNameChange} required />
+        <input type="text" id="name" value={name} onChange={handleNameChange} onClick={handleNameClick} onSelect={handleNameClick} onBlur={handleNameClickBlur} required />
       </div>
 
 
-      <div className={`form-group ${email ? 'focused' : ''}`}>
+      <div className={`form-group ${email ? 'focused' : emailClick === true ? 'focused' : ''}`}>
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" value={email} onChange={handleEmailChange} required />
+        <input type="email" id="email" value={email} onChange={handleEmailChange} onClick={handleEmailClick} onSelect={handleEmailClick} onBlur={handleEmailClickBlur} required />
       </div>
 
 
-      <div className={`form-group ${subject ? 'focused' : ''}`}>
+      <div className={`form-group ${subject ? 'focused' : subjectClick === true ? 'focused' : ''}`}>
         <label htmlFor="subject">Assunto</label>
-        <input type="text" id="subject" value={subject} onChange={handleSubjectChange} required />
+        <input type="text" id="subject" value={subject} onChange={handleSubjectChange} onClick={handleSubjectClick} onSelect={handleSubjectClick} onBlur={handleSubjectClickBlur} required />
       </div>
 
 
-      <div className={`form-group ${message ? 'focused' : ''}`}>
+      <div className={`form-group ${message ? 'focused' : messageClick === true ? 'focused' : ''}`}>
         <label htmlFor="message">Mensagem</label>
-        <textarea id="message" rows="4" value={message} onChange={handleMessageChange} required></textarea>
+        <textarea id="message" rows="4" value={message} onChange={handleMessageChange} onClick={handleMessageClick} onSelect={handleMessageClick} onBlur={handleMessageClickBlur} required></textarea>
       </div>
 
 
@@ -66,7 +103,6 @@ const StyledFormulario = styled.form`
   display: flex;
   flex-direction: column;
   width: 400px;
-  /* box-shadow: 0 0 10px rgba(215, 211, 80, .6); */
   padding: 1rem;
   border-radius: 3px;
   border: 2px solid rgba(215, 211, 80, 0.3);
@@ -84,7 +120,7 @@ const StyledFormulario = styled.form`
       position: absolute;
       top: .5rem;
       left: .5rem;
-      transition: transform 0.2s ease-out, font-size 0.2s ease-out;
+      transition: transform 0.1s ease-out, font-size 0.1s ease-out;
       z-index: 1;
       background-color: white;
       padding: 0 5px 0 5px;
@@ -96,23 +132,33 @@ const StyledFormulario = styled.form`
       width: 100%;
       padding: 1rem;
       font-size: 1rem;
-      border: 1px solid #7c7b7b;
+      border: .5px solid #e6e6e6;
       border-radius: 5px;
-      caret-color: transparent;
       cursor: pointer;
+      outline-color: #9e8f05;
+
+      &:focus {
+        transform: translateY(-.4rem);
+      }
+
+      &:hover {
+        transform: translateY(-.4rem);
+        border-color: #9e8f05;
+      }
     }
     
     .form-group.focused label {
       transform: translateY(-1.3rem);
       font-size: 0.75rem;
-      color: black;
+      color: #9e8f05;
+      border-color: #9e8f05;
+      outline-color: #9e8f05;
     }
 
-    input:focus,
-    textarea:focus {
-      transform: translateY(-.5rem);
-      border-color: #7c7b7b;
-
+    .form-group.focused input,
+    .form-group.focused textarea {
+      border: 1px solid #9e8f05;
+      outline-color: #9e8f05;
     }
 
     .botao {
@@ -124,6 +170,12 @@ const StyledFormulario = styled.form`
       height: 3rem;
       max-width: 6rem;
       cursor: pointer;
+      outline-color: #9e8f05;
+
+      &:hover {
+        transform: translateY(-.4rem);
+        outline-color: #9e8f05;
+      }
     }
 `;
 
--- src/pages/Contact/indexContato.jsx
+++ src/pages/Contact/indexContato.jsx
@@ -5,19 +5,21 @@ import Formulario from '../../components/contatoFormulario';
 
 const Contato = () => {
   return(
-    <StyledDiv>
-      <h1>Contato</h1>
-      <p>Entre em contato conosco deixando sua dúvida, sugestão, crítica 
-        ou elogio.</p>
-      <Formulario />        
-    </StyledDiv>
+    <StyledDivForm>
+      <div className='divContact'>
+        <h1>Contato</h1>
+        <p>Entre em contato conosco deixando sua dúvida, sugestão, crítica 
+          ou elogio.
+        </p>
+        <Formulario />        
+      </div>
+    </StyledDivForm>
   );
 }
 
 
-const StyledDiv = styled.div `
+const StyledDivForm = styled.div `
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: white;
@@ -25,18 +27,29 @@ const StyledDiv = styled.div `
   height: 100%;
   padding: 1rem 0 7rem 0;
 
-  h1 {
-    color: #7c7b7b;
-    margin-bottom: 1rem;
-    font-family: sans-serif;
-    font-size: 25px;
-  }
+  .divContact {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    width: 100%;
+    height: 100%;
+    padding: 1rem 0 7rem 0;
 
-  p {
-    color: #7c7b7b;
-    margin: 2rem 2rem 6rem 2rem;
-    font-family: sans-serif;
-    text-align: justify;
+    h1 {
+      color: #7c7b7b;
+      margin-bottom: 1rem;
+      font-family: sans-serif;
+      font-size: 25px;
+    }
+
+    p {
+      color: #7c7b7b;
+      margin: 1rem 1rem 3rem 1rem;
+      font-family: sans-serif;
+      text-align: center;
+    }
   }
   
 `;
